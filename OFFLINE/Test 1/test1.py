47923ecdf09bfedf68b3f223dc63ce054dca468c
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-17 14:54:44 +0100

    test1.py working file

--- OFFLINE/Test 1/test1.py
+++ OFFLINE/Test 1/test1.py
@@ -11,6 +11,43 @@ engine = create_engine("postgresql://postgres:161086@localhost/test-db-01", echo
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
+# BUILD DATABASE CLASS HERE
+
+@app.route('/')
+def intro1():
+	return render_template('intro1.html')
+
+@app.route('/intro2')
+def intro2():
+	return render_template('intro2.html')
+
+@app.route('/intro3')
+def intro3():
+	return render_template('intro3.html')
+
+@app.route('/intro4')
+def intro4():
+	return render_template('intro4.html')
+
+@app.route('/intro5')
+def intro5():
+	return render_template('intro5.html')
+
+@app.route('/intro6')
+def intro6():
+	return render_template('intro6.html')
+
+@app.route('/intro7')
+def intro7():
+	return render_template('intro7.html')
+
+@app.route('/home')
+def home():
+	return "Hello World!"
+
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug=True)
