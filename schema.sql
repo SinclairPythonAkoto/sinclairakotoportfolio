5dd5f71693200b2e9a08fe5c65c97792d94a368b
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-19 23:05:41 +0100

    schema changed

--- schema.sql
+++ schema.sql
@@ -1,7 +1,32 @@
-DROP TABLE if exists example;
-CREATE TABLE example (
+DROP TABLE if exists review_page;
+CREATE TABLE review_page (
   id SERIAL PRIMARY KEY,
-  info VARCHAR,
-  name VARCHAR,
-  city VARCHAR
+  firstname VARCHAR,
+  lastname VARCHAR,
+  email VARCHAR,
+  review_message VARCHAR
   );
+
+DROP TABLE if exists feedback_page;
+CREATE TABLE feedback_page (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR,
+	experience VARCHAR,
+	functionality VARCHAR,
+	aesthetics VARCHAR,
+	my_cv VARCHAR,
+	my_webapp VARCHAR,
+	outstanding VARCHAR,
+	improve VARCHAR,
+	email VARCHAR
+);
+
+DROP TABLE if exists contactme_page;
+CREATE TABLE contactme_page (
+	id SERIAL PRIMARY KEY,
+	firstname VARCHAR,
+	lastname VARCHAR,
+	email VARCHAR,
+	exposure VARCHAR,
+	message VARCHAR
+);
