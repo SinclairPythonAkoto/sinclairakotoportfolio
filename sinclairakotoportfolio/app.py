f2bee8331e5ea0679479d8405c66eb3095f8c51d
SinclairPythonAkoto <sinclair.python@gmail.com>
2019-08-20 07:41:40 +0100

    file change - import request from flask

--- sinclairakotoportfolio/app.py
+++ sinclairakotoportfolio/app.py
@@ -6,6 +6,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from flask_sqlalchemy import SQLAlchemy
 
+from flask import request
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL") # this connects to heroku database
